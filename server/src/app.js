9fb41e8fea3c40b576b26904e3e9c26d9208c5fd
Shivam Dubey <108024596+Shivam00002@users.noreply.github.com>
2024-02-13 13:38:03 +0530

    search

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,8 @@ require('dotenv').config();
 const connectDb = require("./db/db");
 const app = express()
 const port = process.env.PORT || 8000
-const MensRanking = require('./models/mens')
+const MensRanking = require('./models/mens');
+
 app.use(cors())
 app.use(express.json())
 
@@ -23,6 +24,7 @@ app.post('/mens', async (req, res) => {
     }
 })
 
+
 // get all data
 app.get('/mens', async (req, res) => {
     try {
@@ -32,6 +34,8 @@ app.get('/mens', async (req, res) => {
         res.status(400).send(e)
     }
 })
+
+
 
 // get data by ID
 app.get('/mens/:id', async (req, res) => {
@@ -43,6 +47,32 @@ app.get('/mens/:id', async (req, res) => {
         res.status(500).send(e)
     }
 })
+
+
+// get data by name // search
+app.get('/menbyname/:name', async (req, res) => {
+    try {
+        const name = req.params.name
+        const findbyname = await MensRanking.find({ name });
+        res.send(findbyname)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+
+// get data by limit 
+app.get('/test', async (req, res) => {
+    try {
+        const { limit } = req.query
+        const limitnum = parseInt(limit)
+        const getMenbyLimit = await MensRanking.find().limit(limitnum)
+        res.send(getMenbyLimit)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
 
 //  for update patch
 app.patch('/mens/:id', async (req, res) => {
@@ -56,6 +86,7 @@ app.patch('/mens/:id', async (req, res) => {
         res.status(500).json({ error: "Internal Server Error" });
     }
 })
+
 
 //  for delete by id
 app.delete('/mens/:id', async (req, res) => {
